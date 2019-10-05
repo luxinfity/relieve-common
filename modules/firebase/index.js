748ed9ddcd2253997ce2354a7cad86dc0c13fc97
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-10-05 20:49:21 +0700

    update firebase

--- modules/firebase/index.js
+++ modules/firebase/index.js
@@ -5,9 +5,15 @@ const admin = require('firebase-admin');
 let instance;
 
 exports.initialize = (config) => {
+    let credential;
+
+    if (config.credentialPath) {
+        credential = require(config.credentialPath);
+    }
+
     instance = admin.initializeApp({
-        ...config,
-        credential: admin.credential.cert(config.credential)
+        databaseURL: config.databaseURL,
+        credential: credential ? admin.credential.cert(credential) : undefined
     });
 };
 
