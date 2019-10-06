bc1e7d27e09eacac2d7a2ff5e96ae92e2c494b21
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-10-06 21:51:16 +0700

    revert

--- modules/firebase/index.js
+++ modules/firebase/index.js
@@ -12,8 +12,7 @@ exports.initialize = (config) => {
     };
 
     if (config.credentialPath) {
-        // __dirname,
-        const dir = path.join('../../../..', config.credentialPath);
+        const dir = path.join(__dirname, '../../../..', config.credentialPath);
         credential = require(dir);
         conf.credential = admin.credential.cert(credential);
     }
